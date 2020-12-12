8f232f0c98370a153343476318622aa9cf3bd75f
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2020-12-12 21:33:45 +0100

    fix: Fix typo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,7 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-@ApiTags('users')
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
