74f6e0d0ad5a557fd15a4f3a30b4c9ed1abc0722
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2020-12-12 20:54:42 +0100

    feature: Add basic health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import 'reflect-metadata';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import { TerminusModule } from '@nestjs/terminus';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
-  controllers: [AppController],
+  imports: [TypeOrmModule.forRoot(), UsersModule, TerminusModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {
