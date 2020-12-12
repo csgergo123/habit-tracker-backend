a8ba67504afb0c7f6567d70f55c31e7104c7bda8
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2020-12-12 17:22:24 +0100

    feature: Set the user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, Timestamp } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  email: string;
+
+  @Column({ length: 255 })
+  password: string;
+
+  @Column({ length: 100 })
+  @Column('first_name')
+  firstName: string;
+
+  @Column({ length: 100 })
+  @Column('last_name')
+  lastname: string;
+
+  @Column({ length: 45 })
+  phone: string;
+
+  @Column({ length: 45 })
+  @Column('register_with')
+  registerWith: string;
+
+  @Column('email_verified_at')
+  emailVerifiedAt: Timestamp;
+
+  @Column('updated_at')
+  updatedAt: Timestamp;
+}
