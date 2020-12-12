7cb15296b2e806d7eafe3e8e8169beb8ed6183ea
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2020-12-12 20:19:27 +0100

    feature: Add create user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsNotEmpty()
+  readonly password: string;
+
+  @IsNotEmpty()
+  readonly firstName: string;
+
+  readonly lastName?: string;
+
+  readonly phone?: string;
+
+  @IsNotEmpty()
+  readonly registerWith: string;
+
+  readonly emailVerifiedAt?: string;
+}
