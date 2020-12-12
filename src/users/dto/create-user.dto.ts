09a6c0cbea0bd0f146993d5a52d46ca6baab790a
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2020-12-12 21:22:17 +0100

    feature: Add annotations for create user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,21 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   readonly email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   readonly password: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   readonly firstName: string;
 
+  @ApiProperty({ required: false })
   readonly lastName?: string;
 
+  @ApiProperty({ required: false })
   readonly phone?: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   readonly registerWith: string;
 
+  @ApiProperty()
   readonly emailVerifiedAt?: string;
 }
