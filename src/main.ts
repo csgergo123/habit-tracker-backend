26dac8e5dd1eaa5d42f13e4796d5ee7c3eb71756
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-01-19 20:39:59 +0100

    feature: Allow just whitelisted api body properties

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   const options = new DocumentBuilder()
     .setTitle('Habit tracker API')
