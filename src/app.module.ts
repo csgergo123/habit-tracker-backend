5cd1ac43ada83bba204370ce33693e42f4fe0506
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-01-21 22:02:35 +0100

    feature: Include moduls

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,20 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { HealthController } from './health/health.controller';
 import { HabitModule } from './habit/habit.module';
+import { TodoModule } from './todo/todo.module';
+import { HabitDoneModule } from './habit-done/habit-done.module';
+import { UserMetaModule } from './user-meta/user-meta.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, TerminusModule, HabitModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UsersModule,
+    TerminusModule,
+    HabitModule,
+    TodoModule,
+    HabitDoneModule,
+    UserMetaModule,
+  ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
