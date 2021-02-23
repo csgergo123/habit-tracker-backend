003ac38c385b1a13796f7e2fdaee32ed141f51f0
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-02-23 20:26:17 +0100

    feat: set passper and jwt in user moudle

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/User';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
