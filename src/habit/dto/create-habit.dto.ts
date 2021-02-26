fc9489274753bf6d5372e990206de1bf4e30e256
Csipkes Gergo <csipkesgergo@mailbox.unideb.hu>
2021-02-26 18:05:01 +0100

    fix: create habit dto

--- src/habit/dto/create-habit.dto.ts
+++ src/habit/dto/create-habit.dto.ts
@@ -1,18 +1,11 @@
 import * as moment from 'moment';
 
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { Intensity } from '../entities/intensity.enum';
 import { Regularity } from '../entities/regularity.enum';
 
 export class CreateHabitDto {
-  @ApiProperty({
-    example: 1,
-    description: 'The ID of the user.',
-  })
-  @IsNotEmpty()
-  readonly userId: number;
-
   @ApiProperty({
     example: 'Do the homework',
     description: 'The title of the habit.',
@@ -38,15 +31,17 @@ export class CreateHabitDto {
   @IsEnum(Intensity)
   readonly intensity: Intensity;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     default: 'lightblue',
     description: 'The color of the habit in the frontend.',
   })
+  @IsString()
   readonly color: string | null; // TODO miért nem látja a megadott értéket?
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     default: moment().format('YYYY-MM-DD HH:mm:ss'),
     description: 'The datetime when the habit was created.',
   })
+  @IsString()
   readonly dateAdded: Date | null;
 }
